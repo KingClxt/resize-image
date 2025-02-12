3ad65882e08435b5c037eedc16e7690a373f8507
KingClxt <hassanetinde@gmail.com>
2025-02-12 20:08:54 +0000

    change upload logic

--- backend/main.py
+++ backend/main.py
@@ -1,47 +1,45 @@
 from fastapi import FastAPI, UploadFile, File, Form
-import os
 from PIL import Image
-import shutil
-from fastapi.responses import Response
 from io import BytesIO
+from fastapi.responses import Response
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuration CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.get('/')
 def index():
-    return {"message":"hello"}
+    return {"message": "Hello"}
 
 @app.post('/upload_image')
-async def uploadImage(largeur:int=Form(...), hauteur:int = Form(...), file:UploadFile = File(...)):
-    
-    directory = f"/static/img/{file.filename}"
-    resize_img = None
-    # print(directory)
-    with open(directory, 'wb') as f:
-        shutil.copyfileobj(file.file, f)
+async def upload_image(largeur: int = Form(...), hauteur: int = Form(...), file: UploadFile = File(...)):
+    try:
+        # Lire le contenu du fichier uploadé
+        contents = await file.read()
         
+        # Ouvrir l'image depuis le flux de bytes
+        img = Image.open(BytesIO(contents))
         
-    resize_img = Image.open(directory)
-    nouvelle_taille = (largeur, hauteur)
-    image_redimensionnee = resize_img.resize(nouvelle_taille)
-    
-    # Convertir l'image en bytes
-    img_byte_arr = BytesIO()
-    image_redimensionnee.save(img_byte_arr, format='PNG')
-    img_byte_arr = img_byte_arr.getvalue()
-    
-    os.remove(directory)
-    
-    return Response(content=img_byte_arr, media_type='image/png')
+        # Redimensionner l'image
+        nouvelle_taille = (largeur, hauteur)
+        image_redimensionnee = img.resize(nouvelle_taille)
         
+        # Convertir l'image redimensionnée en bytes
+        img_byte_arr = BytesIO()
+        image_redimensionnee.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
         
-
+        # Renvoyer l'image sous forme de réponse HTTP
+        return Response(content=img_byte_arr, media_type='image/png')
+    
+    except Exception as e:
+        # Gérer les erreurs (par exemple, si le fichier n'est pas une image valide)
+        return {"error": str(e)}
